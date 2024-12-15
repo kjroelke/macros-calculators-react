338180c59671220bda6a00da7bc50056762935d4
kjroelke <kj.roelke@gmail.com>
2024-12-15 11:46:59 -0600

    init shadcn and vite

---
+++ src/Components/BMRCalc.tsx
@@ -0,0 +1,23 @@
+import Button from './Buttons/Button';
+import ClientInfo from './ClientInfo';
+
+import { useMacros } from '../Context/MacroContext';
+import Form from './Form';
+
+export default function BMRCalc() {
+    const {
+        bio: { gender },
+    } = useMacros();
+    return (
+        <Form id='bmr-calculator'>
+            <div className='form__content--gender'>
+                <p>
+                    Current Gender:{' '}
+                    <span className='current-gender font-bold'>{gender}</span>
+                </p>
+                <Button dispatchType='bio/gender'>Toggle Gender</Button>
+            </div>
+            <ClientInfo />
+        </Form>
+    );
+}
