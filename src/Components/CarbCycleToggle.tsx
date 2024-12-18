028fb03a68199056d6c7dfd1c7c2bede0d4f1669
KJ Roelke <kj.roelke@gmail.com>
2024-12-17 22:19:20 -0600

    init carb cycle toggle component

---
+++ src/Components/CarbCycleToggle.tsx
@@ -0,0 +1,25 @@
+import { Switch } from '@/Components/ui/switch';
+import { useMacros } from '@/hooks/useMacros';
+
+export default function CarbCycleToggle() {
+    const { dispatch, carbCycle } = useMacros();
+    return (
+        <div className='flex justify-between items-center gap-2'>
+            <p className='font-bold text-primary text-lg'>Carb Cycling</p>
+            <div className='flex items-center gap-2'>
+                <label htmlFor='carbCycle'>Off</label>
+                <Switch
+                    id='carbCycle'
+                    checked={carbCycle}
+                    onChange={(checked) =>
+                        dispatch({
+                            type: 'carbCycle',
+                            payload: checked,
+                        })
+                    }
+                />
+                <label htmlFor='carbCycle'>On</label>
+            </div>
+        </div>
+    );
+}
