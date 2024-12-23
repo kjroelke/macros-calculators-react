987c1a86ad4d612799ccab93781dec0db89bf460
KJ Roelke <kj.roelke@gmail.com>
2024-12-22 22:14:44 -0600

    finish initial class refactor

--- src/lib/Math/calcCalorieGoal.ts
+++ src/lib/Math/calcCalorieGoal.ts
@@ -4,21 +4,22 @@
  * @param tdee Total Daily Energy Expenditure
  * @param deficit  Percentage of calories to cut
  * @param bmr Basal Metabolic Rate
- * @returns Calorie Goal or 'Too low!'
+ * @returns Calorie Goal
+ * @throws Error if calories are too low
  */
 export function calcCalorieGoal(
     tdee: number,
     deficit: number,
     bmr: number,
-): number | string {
-    let calories: number | string = 0;
+): number {
+    let calories: number = 0;
     const isCut = 1.0 > deficit;
     const isMaintenance = 1.0 === deficit;
     const isBulk = 1.0 < deficit;
     if (isCut) {
         calories = Math.round(tdee - tdee * deficit);
         if (calories < bmr) {
-            calories = 'Too low!';
+            throw new Error('Too low!');
         }
     } else if (isMaintenance) {
         calories = tdee;
--- src/lib/Math/calcMacros.ts
+++ src/lib/Math/calcMacros.ts
@@ -1,147 +1,119 @@
 import { AppState } from '../types/types.app';
-import { Macros, macroState, modifiers } from '../types/types.macros';
-import { Person } from '../types/types.person';
+import { Macros, macroState } from '../types/types.macros';
 import { calcBMR } from './calcBMR';
 import { calcCalorieGoal } from './calcCalorieGoal';
 
 class MacroCalculator {
-    private bio: Person;
-    private modifiers: modifiers;
-    private macros: macroState;
+    /** The Passed state */
+    private state: AppState;
+    /**
+     * The calculated BMR
+     */
+    private bmr: number;
+
+    /**
+     * The calculated TDEE
+     */
+    private tdee: number;
+
+    /**
+     * The calculated Calorie Goal
+     */
     private calorieGoal: number | string;
-    private carbCycle: boolean;
 
     constructor(state: AppState) {
-        this.bio = state.bio;
-        this.modifiers = state.modifiers;
-        this.macros = state.macros;
-        this.calorieGoal = state.calorieGoal;
-        this.carbCycle = state.carbCycle;
+        this.state = state;
+        this.bmr = calcBMR(state.bio);
+        this.tdee = Math.round(this.bmr * state.modifiers.activity);
+        this.calorieGoal = 0;
     }
     calcAllMacros(): AppState {
-        const bmr = calcBMR(this.bio);
-        const newTdee = Math.round(bmr * this.modifiers.activity);
-        this.calorieGoal = calcCalorieGoal(
-            newTdee,
-            this.modifiers.deficit,
-            bmr,
-        );
-        this.macros = calcMacros(
-            this.macros,
-            this.modifiers,
-            this.bio,
-            this.calorieGoal,
-            this.carbCycle,
-        );
-        return {
-            ...state,
-            bmr,
-            tdee: newTdee,
-            calorieGoal: this.calorieGoal,
-            macros: this.macros,
-        };
-    }
-}
-
-
-export function calcAllMacros(state: AppState): AppState {
-    const bmr = calcBMR(state.bio);
-    const newTdee = Math.round(bmr * state.modifiers.activity);
-    const newState = {
-        ...state,
-        bmr,
-        tdee: newTdee,
-        calorieGoal: calcCalorieGoal(
-            newTdee,
-            state.modifiers.deficit,
-            bmr,
-        ),
-        macros: calcMacros(
-            state.macros,
-            state.modifiers,
-            state.bio,
-            state.calorieGoal
-            state.carbCycle,
-        ),
-    }
-    return newState;
-}
-
-
-export function calcMacros(
-    macros: macroState,
-    modifiers: modifiers,
-    bio: Person,
-    calorieGoal: number | string,
-    carbCycle: boolean,
-): macroState | { lowCarb: macroState; highCarb: macroState } {
-    if (false === carbCycle) {
         try {
-            macros.proteins = calcProteins(
-                macros.proteins,
-                modifiers.protein,
-                bio,
-                Number(calorieGoal),
+            this.calorieGoal = calcCalorieGoal(
+                this.tdee,
+                this.state.modifiers.deficit,
+                this.bmr,
             );
-            macros.fats = calcFats(macros.fats, Number(calorieGoal));
-            macros.carbs = calcCarbs(macros, Number(calorieGoal));
-            return macros;
+            const proteins = this.calcProteins();
+            const fats = this.calcFats();
+            const carbs = this.calcCarbs();
+            return {
+                ...this.state,
+                bmr: this.bmr,
+                tdee: this.tdee,
+                calorieGoal: this.calorieGoal as number,
+                macros: {
+                    proteins,
+                    fats,
+                    carbs,
+                },
+            };
         } catch (error) {
-            console.error(error);
-            alert(error);
-            return macros;
+            this.calorieGoal = error as string;
+            return {
+                ...this.state,
+                calorieGoal: this.calorieGoal,
+            };
         }
     }
-}
 
-function calcProteins(
-    proteins: Macros,
-    modifier: number,
-    person: Person,
-    calorieGoal: number,
-): Macros {
-    if (typeof calorieGoal === 'string') {
-        throw new Error('Calorie Goal too low!');
+    private calcProteins(): Macros {
+        const {
+            bio: person,
+            modifiers: { protein: modifier },
+        } = this.state;
+        const { proteins } = this.state.macros as macroState;
+        let { grams, calories } = proteins as Macros;
+        grams = Math.round(person.weight * modifier);
+        calories = Math.round(grams * 4);
+        const percentage = Math.round((calories / this.calorieGoal) * 100);
+        const proteinMacros = {
+            percentage: percentage,
+            grams: grams,
+            calories: calories,
+        };
+        return proteinMacros;
     }
-    let { grams, calories } = proteins;
-    grams = Math.round(person.weight * modifier);
-    calories = Math.round(grams * 4);
-    const percentage = Math.round((calories / calorieGoal) * 100);
-    const proteinMacros = {
-        percentage: percentage,
-        grams: grams,
-        calories: calories,
-    };
-    return proteinMacros;
+
+    private calcFats(): Macros {
+        const { fats } = this.state.macros as macroState;
+        let { grams, calories } = fats;
+        calories = Math.round((30 / 100) * this.calorieGoal);
+        grams = Math.round(calories / 9);
+        const fatMacros = {
+            grams: grams,
+            calories: calories,
+            percentage: 30,
+        };
+        return fatMacros;
+    }
+
+    private calcCarbs(): Macros {
+        const goal = this.state.calorieGoal as number;
+        const macros = this.state.macros as macroState;
+        let {
+            carbs: { grams: cGrams, percentage: cPercent, calories: cCals },
+        } = macros;
+        const {
+            fats: { calories: fCals },
+            proteins: { calories: pCals },
+        } = macros;
+        cCals = Math.round(goal - fCals - pCals);
+        cGrams = Math.round(cCals / 4);
+        cPercent = Math.round((cCals / goal) * 100);
+        const carbMacros = {
+            calories: cCals,
+            grams: cGrams,
+            percentage: cPercent,
+        };
+        return carbMacros;
+    }
+
+    private calcLowCarbs(): macroState {}
+    private calcHighCarbs(): macroState {}
 }
 
-function calcFats(fats: Macros, calorieGoal: number): Macros {
-    let { grams, calories } = fats;
-    calories = Math.round((30 / 100) * calorieGoal);
-    grams = Math.round(calories / 9);
-    const fatMacros = {
-        grams: grams,
-        calories: calories,
-        percentage: 30,
-    };
-    return fatMacros;
-}
-
-function calcCarbs(macros: macroState, goal: number): Macros {
-    let {
-        carbs: { grams: cGrams, percentage: cPercent, calories: cCals },
-    } = macros;
-    const {
-        fats: { calories: fCals },
-        proteins: { calories: pCals },
-    } = macros;
-    cCals = Math.round(goal - fCals - pCals);
-    cGrams = Math.round(cCals / 4);
-    cPercent = Math.round((cCals / goal) * 100);
-    const carbMacros = {
-        calories: cCals,
-        grams: cGrams,
-        percentage: cPercent,
-    };
-    return carbMacros;
+export function calcAllMacros(state: AppState): AppState {
+    const calculator = new MacroCalculator(state);
+    return calculator.calcAllMacros();
 }
