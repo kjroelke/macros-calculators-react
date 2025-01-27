bd7057b89d0c8a83192039f5fd00306ceeee9b81
KJ Roelke <kj.roelke@gmail.com>
2025-01-26 21:20:07 -0600

    fix slider Fixes #30

--- src/ui/AppInternals.tsx
+++ src/ui/AppInternals.tsx
@@ -5,14 +5,14 @@ import ProteinInput from '@/Components/ProteinInput';
 
 export default function AppInternals() {
     return (
-        <div className='mx-auto my-0 grid md:grid-cols-2 gap-y-10 gap-x-5 md:py-10 md:px-2 max-w-screen-xl'>
-            <div className='md:col-start-2 md:row-start-1 md:col-span-full relative'>
-                <Output />
-            </div>
+        <div className='mx-auto my-0 grid md:grid-cols-2 md:flex-row-reverse gap-y-10 gap-x-5 md:py-10 md:px-2 max-w-screen-xl'>
             <div className='md:col-start-1 md:row-start-1 md:row-span-full flex flex-col gap-5'>
                 <BMRCalc />
                 <Modifiers />
                 <ProteinInput />
+            </div>
+            <div className='md:col-start-2 md:row-start-1 md:col-span-full relative'>
+                <Output />
             </div>
         </div>
     );
